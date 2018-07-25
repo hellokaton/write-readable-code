8e032000eb807cb751e717531a1d6eb103f61327
biezhi <biezhi.me@gmail.com>
2018-07-25 21:31:22 +0800

    :sparkles: add chapter 10 code

--- src/main/java/chapter_10/Example3.java
+++ src/main/java/chapter_10/Example3.java
@@ -10,11 +10,12 @@ public class Example3 {
 
     String username;
 
-    public void buildName() {
+    public void method1() {
         username = "abc";
+        System.out.println(method2());
     }
 
-    public boolean checkName() {
+    public boolean method2() {
         return null != username && username.length() > 6;
     }
 
