8b07f2a623ca6af6af9b18cc911bcb5b6598d88f
biezhi <biezhi.me@gmail.com>
2018-07-25 21:08:04 +0800

    :sparkles: add chapter 10 code

--- src/main/java/chapter_10/Example4.js
+++ src/main/java/chapter_10/Example4.js
@@ -1,4 +1,4 @@
-var submmitted = false;
+var submmitted = false; // 全局变量
 
 var submit_form = function (form_name) {
     if(submmitted) {
@@ -8,3 +8,12 @@ var submit_form = function (form_name) {
     // ...
     submmitted = true;
 };
+
+
+var f = function () {
+    // 定义 i 的时候没有使用 var 声明
+    for (i = 0; i < 10; i++){
+        //
+    }
+};
+f();
