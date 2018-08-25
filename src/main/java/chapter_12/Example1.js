cc3607e80fc0e8c6ffda94fd9d63e71e5de2cf40
biezhi <biezhi.me@gmail.com>
2018-08-25 16:18:47 +0800

    :sparkles: add chapter 12 code

--- src/main/java/chapter_12/Example1.js
+++ src/main/java/chapter_12/Example1.js
@@ -1,16 +1,18 @@
 var vote_change = function (old_vote, new_vote) {
-  var score = get_score();
-  if (new_vote !== old_vote) {
-    if (new_vote === 'Up') {
-      score += (old_vote === 'Up' ? 2 : 1);
-    } else if (new_vote === 'Down') {
-      score -= (old_vote === 'Up' ? 2 : 1);
-    } else if (new_vote === '') {
-      score += (old_vote === 'Up' ? -1 : 1);
+    var weight = get_weight();
+    if (new_vote !== old_vote) {
+        if (new_vote === 'Up') {
+            weight += (old_vote === 'Up' ? 2 : 1);
+        } else if (new_vote === 'Down') {
+            weight -= (old_vote === 'Up' ? 2 : 1);
+        } else if (new_vote === '') {
+            weight += (old_vote === 'Up' ? -1 : 1);
+        }
+        set_weight(weight);
     }
-    set_score(score);
-  }
 };
 
-function get_score(){}
-function set_score(score){}
+function get_weight() {
+}
+function set_weight(weight) {
+}
